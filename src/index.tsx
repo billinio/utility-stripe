835d173b81062663f23d7d215145b28e8be39c0a
Steve McKeogh <steve.mckeogh@gmail.com>
2020-12-21 12:41:47 +0700

    fix(stripe): loaded and wrapped

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { Routes } from "routes";
 import React from "react";
 import ReactDOM from "react-dom";
+import { config } from "system/config";
+
+const stripe = loadStripe(config.stripe.publicKey);
 
 import "./css/global.css";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Elements stripe={stripe}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Elements>,
   document.getElementById("root"),
 );
